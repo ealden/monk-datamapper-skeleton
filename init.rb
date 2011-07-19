9455066f96070165b4ee58d0e46099503c130a4c
Ealden Esto E. Escanan <ealden@teamcodeflux.com>
2011-07-20 00:09:39 +0800

    Connect to PostgreSQL via DataMapper on startup
    We specify PostgreSQL connetion details in settings.example.yaml and
    connect to it on startup.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@teamcodeflux.com>

--- init.rb
+++ init.rb
@@ -11,16 +11,27 @@ end
 require "monk/glue"
 require "haml"
 require "sass"
+require "data_mapper"
 
 class Main < Monk::Glue
   set :app_file, __FILE__
   use Rack::Session::Cookie
 end
 
+host = monk_settings(:postgresql)[:host]
+port = monk_settings(:postgresql)[:port]
+username = monk_settings(:postgresql)[:username]
+password = monk_settings(:postgresql)[:password]
+database = monk_settings(:postgresql)[:database]
+
+DataMapper.setup(:default, "postgres://#{username}:#{password}@#{host}:#{port}/#{database}")
+
 # Load all application files.
 Dir[root_path("app/**/*.rb")].each do |file|
   require file
 end
+
+DataMapper.finalize
 
 if defined? Encoding
   Encoding.default_external = Encoding::UTF_8
