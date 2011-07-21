0e209dcfee22ccca7cf528b436f940a98b75349a
Ealden Esto E. Escanan <ealden@teamcodeflux.com>
2011-07-21 17:25:21 +0800

    load_data test helper to load data from a Ruby data script file
    This allows to fine tune test data for tests.  Tests should only
    call load_data with the path to the test data script and it will
    be loaded each time a test method is executed.
    
    This also loads common Ruby data scripts before loading the
    data script specified.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@teamcodeflux.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,22 @@ class Test::Unit::TestCase
   def app
     Main.new
   end
+
+  def load_data(*paths)
+    data_dir = File.join(File.dirname(__FILE__), "..", "data", "datamapper")
+
+    load_common_data(data_dir)
+
+    load File.expand_path(File.join(data_dir, ENV['RACK_ENV'], paths))
+  end
+
+private
+
+  def load_common_data(data_dir)
+    common_data_path = File.expand_path(File.join(data_dir, "common", "*.rb"))
+
+    Dir[common_data_path].each do |file|
+      load file unless file =~ /^-/
+    end
+  end
 end
